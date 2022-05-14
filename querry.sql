7ea8e952848a08bb87b2142951e9479e3f607117
selimhanmrl <45050457+selimhanmrl@users.noreply.github.com>
2022-05-14 11:57:05 +0300

    Add files via upload

---
+++ querry.sql
@@ -0,0 +1,23 @@
+--select title, description from film;
+--select * from film where length > 60 and length < 75;
+
+--select * from film where rental_rate > 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99;
+
+--select last_name from customer where first_name = 'Mary';
+
+--select * from film where not length > 50 and not (rental_rate = 2.99 or rental_rate =4.99);
+
+--select * from film where replacement_cost BETWEEN 12.99 and 16.99;
+
+--select first_name,last_name from actor where first_name IN ('Nick','Ed','Penelope');
+
+--select * from film where rental_rate IN ( 0.99,2.99, 4.99) and replacement_cost  in(12.99,15.99,28.99);
+
+select country from country where country like 'A%a';
+
+select country from country where country like '_____%n';
+
+select title from film where title ilike 'T%T%T%T%';
+
+select title from film where title ilike 'C%' and length >90 and rental_rate = 2.99
+
